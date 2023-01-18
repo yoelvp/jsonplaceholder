c3bea7c5a64040cb682f0dfce19593a7fc3a02de
yoelvp <yoelvp73@gmail.com>
2023-01-17 22:26:24 -0500

    route wrapper and header

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -3,16 +3,17 @@ import { Routes, Route } from 'react-router-dom'
 import Header from '../components/Header'
 import Home from '../pages/home/Home'
 import Posts from '../pages/posts/Posts'
+import { Wrapper } from '../ui/core'
 
 const Routing = () => {
   return (
-    <>
+    <Wrapper>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/posts' element={<Posts />} />
       </Routes>
-    </>
+    </Wrapper>
   )
 }
 
