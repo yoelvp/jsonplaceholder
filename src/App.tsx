29debc8646826c04d0b481a5e7ba459c2d5c8a16
yoelvp <yoelvp73@gmail.com>
2023-10-24 11:03:31 -0500

    modify: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import type { FC } from "react"
+import type { FC } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import Routing from '@/router/Routing'
+import { GlobalStyles } from '@/ui'
+import { theme } from '@/lib/theme'
 
 const App: FC = () => {
   return (
-    <h1>
-      Hello world
-    </h1>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Routing />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
