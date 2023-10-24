d9c6df1efb258f83692d75dd2697fb354111fc0c
yoelvp <yoelvp73@gmail.com>
2023-10-24 11:00:20 -0500

    add custom theme

---
+++ src/lib/theme.ts
@@ -0,0 +1,41 @@
+export const theme = {
+  colors: {
+    primary: '#00A9FF',
+    secondary: '#1D5B79',
+    blue: '#1F4172',
+    green: '#A7D397',
+    red: '#C70039',
+    yellow: '#FCE09B',
+    white: '#FFFFFF',
+    gray: '#F1EFEF'
+  },
+  sizes: {
+    small: {
+      x: '2rem',
+      y: '0.25rem'
+    },
+    medium: {
+      x: '2rem',
+      y: '0.5rem'
+    },
+    large: {
+      x: '2rem',
+      y: '0.75rem'
+    },
+  },
+  rounded: {
+    small: '0.25rem',
+    medium: '0.5rem',
+    large: '1rem'
+  },
+  fontSizes: {
+    'md': '16px',
+    'lg': '18px',
+    'xl': '24px',
+    '2xl': '',
+    '3xl': '',
+    '4xl': '',
+    '5xl': '',
+    '6xl': ''
+  }
+}
