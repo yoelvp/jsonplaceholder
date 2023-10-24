9a146c42b9878990583f5a66a8a60a14bf0935af
yoelvp <yoelvp73@gmail.com>
2023-10-24 11:00:04 -0500

    add absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@layouts': '/src/components/layouts',
-      '@forms': '/src/components/forms',
-      '@assets': '/src/assets',
-      '@hooks': '/src/hooks',
-      '@services': '/src/services',
-      '@lib': '/src/lib',
-      '@router': '/src/router',
-      '@web': '/src/pages/web',
-      '@dashboard': '/src/pages/dashboard',
-      '@auth': '/src/auth',
-      '@context': '/src/context',
-      '@utils': '/src/utils',
-      '@dto': '/src/dto'
+      '@': '/src'
     }
   }
 })
