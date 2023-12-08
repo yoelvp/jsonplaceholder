714514ec7b63e218cb01eb75dd7cd4b68b4bd656
yoelvp <yoelvp73@gmail.com>
2023-12-08 10:44:41 -0500

    change homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,37 @@
 import type { FC } from 'react';
 
+import { usePosts } from '@/hooks/usePosts';
+import PostItem from './components/PostItem';
+import { Box, Button, Container, Grid, Heading, PhoneCase } from '@/ui';
+
 const Homepage: FC = () => {
+  const { posts, isLoading, error, fetchNextPage } = usePosts()
+
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <Container>
+      <PhoneCase>
+        <Heading $fontSize='28' $color='secondary'>
+          {'{JSON}'} Placeholder
+        </Heading>
+
+        {error && (
+          <div>{error}</div>
+        )}
+        <Grid>
+          {posts.map(post => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </Grid>
+        <Box $paddingY='16'>
+          {!isLoading && (
+            <Button onClick={fetchNextPage} $background='green' $color='dark'>
+              Load more
+            </Button>
+          )}
+          {isLoading && 'Loading more results...'}
+        </Box>
+      </PhoneCase>
+    </Container>
   )
 }
 
